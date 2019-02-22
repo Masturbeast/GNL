22ee06299cedbc9f5fba5099e63ec2ea85e0fa6e
Masturbeast <atep@e2r2p18.42.fr>
2019-02-22 18:03:41 +0100

    a refaire

--- get_next_line.c
+++ get_next_line.c
@@ -5,62 +5,126 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: atep <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/12/18 14:19:28 by atep              #+#    #+#             */
-/*   Updated: 2018/12/19 16:58:26 by atep             ###   ########.fr       */
+/*   Created: 2019/02/19 14:49:51 by atep              #+#    #+#             */
+/*   Updated: 2019/02/22 17:01:40 by atep             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
-char    *ft_read_fd(const int fd)
+t_tab	*ft_init_struct(int fd, t_tab **lst)
 {
-	char *tab;
+	t_tab	*tmp;
 
-	while (read(fd, &buf, BUFF_SIZE))
+	tmp = *lst;
+	while (tmp != NULL)
 	{
-		if (!(tab = ft_strdupcat(tab, buf)))
-				return (NULL);
-		if (buf = '\n')
-			return(tab);
+		if (tmp->fd == fd)
+			return (tmp);
+		tmp = tmp->next;
 	}
-	return (1);
+	if (tmp == NULL)
+	{
+		if ((tmp = (t_tab*)malloc(sizeof(t_tab))) == NULL)
+			return (NULL);
+		tmp->fd = fd;
+		tmp->str = NULL;
+		tmp->next = *lst;
+		*lst = tmp;
+	}
+	return (tmp);
 }
 
-char		*ft_strdupcat(char *str, char buf)
+char	*ft_strdupcat(char *str, char **line)
 {
-	int		i;
-	int		len_str;
-	char	*cpy;
+	char	*tmp;
+	int		size;
 
-	len_str = ft_strlen(str);
-	i = 0;
-	if (!(cpy = (char*)malloc(sizeof(char) * len_str + 2))) //sizeof BUFF_SIZE
-		return (NULL);
-	while (i < len_str)
+	size = ft_strlen(str) - ft_strlen(ft_strchr(str, '\n'));
+	line[0] = ft_strsub(str, 0, size);
+	tmp = ft_strsub(str, size + 1, ft_strlen(str));
+	free(str);
+	if (ft_strlen(tmp) != 0)
+		str = ft_strdup(tmp);
+	else
+		str = NULL;
+	free(tmp);
+	return (str);
+}
+
+char	*ft_catfrombuff(char *buff, char *str, char **line)
+{
+	char	*tmp;
+
+	if (str == NULL)
+		str = ft_strdup(buff);
+	else
 	{
-		cpy[i] = str[i];
-		i++;
+		tmp = ft_strdup(str);
+		free(str);
+		str = ft_strjoin(tmp, buff);
+		free(tmp);
 	}
-	cpy[i] = buf;
-	cpy[i + 1] = '\0';
-	str = cpy;
+	if (ft_strchr(str, '\n') != NULL)
+		str = ft_strdupcat(str, &line[0]);
+	else
+		line[0] = NULL;
+	return (str);
+}
+
+char	*ft_endofline(char *str, char **line)
+{
+	if (str != NULL)
+	{
+		if (ft_strchr(str, '\n') != NULL)
+			str = ft_strdupcat(str, &line[0]);
+		else if (str != NULL)
+		{
+			line[0] = ft_strdup(str);
+			free(str);
+			str = NULL;
+		}
+		else
+			line[0] = NULL;
+	}
+	else
+		line[0] = NULL;
 	return (str);
 }
 
 int		get_next_line(const int fd, char **line)
 {
-	static char *str;
-	int 		i;
-
-	i = 0;
-	while (str = ft_read_fd(fd))
-	{	
-		line[i] = str;;
-		i++;
-		return (1);
-	if (ft_read_fd(fd) = 1)
-		return (0);
-	if (!(ft_read_fd(fd)))
+	static t_tab	*lst;
+	int				i;
+	char			buff[BUFF_SIZE + 1];
+	t_tab			*tmp;
+	
+//	printf("0\n");
+	if (fd < 0 || !line || read(fd, buff, 0) < 0)
 		return (-1);
+//	printf("1\n");
+	tmp = ft_init_struct(fd, &lst);
+//	printf("2\n");
+	while ((i = read(fd, buff, BUFF_SIZE)) > 0)
+	{
+//		printf("3\n");
+		buff[i] = '\0';
+//		printf("4\n");
+		tmp->str = ft_catfrombuff(buff, tmp->str, &line[0]);
+		if(line[0] != NULL)
+			return (1);
+//		printf("5\n");
+//		printf("6\n");
 	}
+//	printf("7\n");
+	if (i == -1)
+		return (i);
+	tmp->str = ft_endofline(tmp->str, &line[0]);
+//	printf("8\n");
+	//printf("line[0] = %s\n", line[0]);
+	if (line[0] != NULL)
+		return (1);
+//	printf("9\n");
+	return (0);
 }
