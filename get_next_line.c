26e9b8bab76b3e01a7e909008dc596abf6f8d7ac
Masturbeast <atep@e3r10p20.42.fr>
2019-02-28 15:57:45 +0100

    final push

--- get_next_line.c
+++ get_next_line.c
@@ -3,72 +3,106 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: soel-kar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: atep <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/12/21 14:25:26 by soel-kar          #+#    #+#             */
-/*   Updated: 2019/02/28 10:33:00 by atep             ###   ########.fr       */
+/*   Created: 2019/02/19 14:49:51 by atep              #+#    #+#             */
+/*   Updated: 2019/02/28 10:32:04 by atep             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
-int					errors(int fd, char **line)
+t_tab	*ft_init_struct(int fd, t_tab **lst)
 {
-	if (fd < 0)
+	t_tab	*tmp;
+
+	tmp = *lst;
+	while (tmp != NULL)
+	{
+		if (tmp->fd == fd)
+			return (tmp);
+		tmp = tmp->next;
+	}
+	if (tmp == NULL)
+	{
+		if ((tmp = (t_tab*)malloc(sizeof(t_tab))) == NULL)
+			return (NULL);
+		tmp->fd = fd;
+		tmp->str = NULL;
+		tmp->next = *lst;
+		*lst = tmp;
+	}
+	return (tmp);
+}
+
+int		ft_get_line(char *buff, char **str, char **line)
+{
+	char	*tmp;
+
+	tmp = *str;
+	if ((*str = ft_strjoin(tmp, buff)) == NULL)
+	{
+		*str = tmp;
 		return (-1);
-	if (line == NULL)
-		return (-1);
+	}
+	if (tmp)
+		free(tmp);
+	if ((tmp = ft_strchr(*str, '\n')) != NULL)
+	{
+		if ((*line = ft_strsub(*str, 0, tmp - (*str))) == NULL)
+			return (-1);
+		buff = *str;
+		if (tmp[1] && (*str = ft_strdup(tmp + 1)) == NULL)
+			return (-1);
+		else if (!tmp[1])
+			*str = NULL;
+		free(buff);
+		return (1);
+	}
 	return (0);
 }
 
-int					get_line(int fd, int ret, char **line, char **str)
+int		ft_endoffile(char **str, char **line)
 {
-	int				size;
-	char			*curr_line;
-
-	size = 0;
-	while (str[fd][size] != '\n' && str[fd][size] != '\0')
-		size++;
-	if (str[fd][size] == '\n')
+	if (!ft_strlen(*str))
 	{
-		*line = ft_strsub(str[fd], 0, size);
-		curr_line = ft_strdup(str[fd] + size + 1);
-		free(str[fd]);
-		str[fd] = curr_line;
-		if (str[fd][0] == '\0')
-			ft_strdel(&str[fd]);
+		free(*str);
+		*str = NULL;
+		return (0);
 	}
-	else if (str[fd][size] == '\0')
+	if (ft_strchr(*str, '\n') != NULL)
+		ft_get_line(NULL, str, line);
+	else
 	{
-		if (ret == BUFF_SIZE)
-			return (get_next_line(fd, line));
-		*line = ft_strdup(str[fd]);
-		ft_strdel(&str[fd]);
+		if ((*line = ft_strdup(*str)) == NULL)
+			return (-1);
+		free(*str);
+		*str = NULL;
 	}
 	return (1);
 }
 
-int					get_next_line(const int fd, char **line)
+int		get_next_line(const int fd, char **line)
 {
-	static char		*str[255];
-	char			buf[BUFF_SIZE + 1];
-	int				ret;
-	char			*tmp_line;
-
-	if (errors(fd, line) == -1)
+	static t_tab	*lst = NULL;
+	long			i;
+	char			buff[BUFF_SIZE + 1];
+	t_tab			*tmp;
+	
+	if (fd < 0 || !line || read(fd, buff, 0) < 0)
 		return (-1);
-	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
+	if (!(tmp = ft_init_struct(fd, &lst)))
+		return (-1);
+	ft_bzero(buff, BUFF_SIZE + 1);
+	while ((i = read(fd, buff, BUFF_SIZE)) > 0)
 	{
-		buf[ret] = '\0';
-		if (str[fd] == NULL)
-			str[fd] = ft_strnew(0);
-		tmp_line = ft_strjoin(str[fd], buf);
-		free(str[fd]);
-		str[fd] = tmp_line;
+		buff[i] = '\0';
+		if ((i = ft_get_line(buff, &tmp->str, line)))
+			return ((int)i);
+		ft_bzero(buff, BUFF_SIZE + 1);
 	}
-	if (ret < 0)
-		return (-1);
-	if (ret == 0 && (str[fd] == NULL || str[fd] == '\0'))
-		return (0);
-	return (get_line(fd, ret, line, str));
+	if (i == 0 && tmp->str != NULL)
+		return (ft_endoffile(&tmp->str, line));	
+	return ((i < 0) ? -1 : 0);
 }
